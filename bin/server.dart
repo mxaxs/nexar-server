b1b25316324fdf353d5dc540e3ab5cd289f386e8
Marcio Santos <santos.marcioalex@gmail.com>
2022-08-25 20:49:07 -0300

    DB added

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:postgres/postgres.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
@@ -7,14 +9,36 @@ import 'package:shelf_router/shelf_router.dart';
 // Configure routes.
 final _router = Router()
   ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
+  ..get('/echo/<message>', _echoHandler)
+  ..post('/calc', _calcHandler);
 
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
+FutureOr<Response> _rootHandler(Request req) async {
+  var connection = PostgreSQLConnection("db", 5432, "postgres",
+      username: "postgres", password: "postgres");
+  await connection.open().then(
+        (value) => print("DB CONNECTED!"),
+      );
+  List<List<dynamic>> results = await connection.query("SELECT * FROM users");
+
+  for (final row in results) {
+    var a = row[0];
+    var b = row[1];
+    var c = row[2];
+    print("$a $b $c");
+  }
+
+  return Response.ok('Estes são os resultdos \n ${results.toString()}\n');
 }
 
 Response _echoHandler(Request request) {
   final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
+FutureOr<Response> _calcHandler(Request request) async {
+  final body = await request.readAsString();
+  final message = body.toString();
+
   return Response.ok('$message\n');
 }
 
